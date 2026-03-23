158f89b03f42d7460d2796e900ad6a3264f28f48
Edder19 <edderpatino1@gmail.com>
2026-03-23 18:31:55 -0500

    changes hello world by class

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Helloworld{
+    class Program{
+     
+        static void Main(){
+              var Name="jone";
+              Name="1 jone";
+            Console.WriteLine($"Hello World: {Name}");
+            Console.WriteLine();
+        } 
+    }
+}
